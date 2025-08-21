0e65c0e277ab26cb32a9242d854d761e1e92d14e
max-ribeiro <max_ribeiro@live.com>
2025-08-20 23:11:39 -0300

    fix: ajustes placeholder de busca

--- resources/js/components/Client/Clients.vue
+++ resources/js/components/Client/Clients.vue
@@ -69,14 +69,14 @@ export default {
                 <!-- Search -->
                 <div class="flex flex-row items-center justify-between p-[16px]">
                     <div class="flex items-center w-1/3">
-                        <label for="default-search" class="sr-only">Buscar cliente</label>
+                        <label for="default-search" class="sr-only">Buscar contato</label>
                         <div class="relative w-80 h-9">
                             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                 <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                 </svg>
                             </div>
-                            <input type="search" id="default-search" placeholder="Buscar cliente" v-model="searchText"
+                            <input type="search" id="default-search" placeholder="Buscar contato" v-model="searchText"
                                 class="block w-full h-full ps-10 text-sm text-gray-900 border border-[#E1E1E1] rounded-lg bg-[#F8F8F8] focus:ring-blue-500 focus:border-blue-500"
                             />
                         </div>
