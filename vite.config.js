0eb37f9bb8fb894cc3ee662d1e024101cecd74d3
max-ribeiro <max_ribeiro@live.com>
2025-08-21 08:48:38 -0300

    fix: ajuste nas conexão do vite

--- vite.config.js
+++ vite.config.js
@@ -27,4 +27,13 @@ export default defineConfig({
             },
         })
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost', 
+            port: 5173,
+        },
+    },
 });
